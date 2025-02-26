080dfdab237fab244d326bbfa9f2adb9efd4af9a
zhongyah <1016509070@qq.com>
2025-02-27 00:09:50 +0800

    update

--- model/deberta/deberta.py
+++ model/deberta/deberta.py
@@ -787,7 +787,10 @@ class DisentangledSelfAttention(nn.Module):
         
         if rel_att is not None:
             if past_key_value is not None:
-                att_shape = None
+                att_shape = rel_att.shape[:-1] + (past_key_value_length,)
+                prefix_att = torch.zeros(*att_shape).to(rel_att)
+                rel_att = torch.cat([prefix_att, rel_att], dim = -1)
+                attention_scores += rel_att
             else:
                 attention_scores += rel_att
                 
@@ -841,6 +844,9 @@ class DisentangledSelfAttention(nn.Module):
             query_layer.shape = key_layer.shape = value_layer.shape = [bz, num_heads, seq_len, head_size]
             relative_pos.shape = [bz, seq_len, seq_len]
             rel_embeddings.shape = [2k, d]
+            
+        ##Return:
+            相对位置的注意力分数: scores, scores.shape = [bz, num_heads, q_len, k_len]
         '''
         
         if relative_pos is None:
@@ -964,8 +970,48 @@ class DebertaEmbeddings(nn.Module):
         self.embedding_size = getattr(config, "embedding_size", config.hidden_size)
         self.word_embeddings = nn.Embedding(config.vocab_size, self.embedding_size, padding_idx=pad_token_id)
         
+        self.position_biased_input = getattr(config, "position_biased_input", True)
+        
+        if not self.position_biased_input:
+            self.position_embeddings = None
+        else:
+            self.position_embeddings = nn.Embedding(config.max_position_embeddings, self.embedding_size)
+
+        if config.type_vocab_size>0:
+            self.token_type_embeddings = nn.Embedding(config.type_vocab_size, self.embedding_size)
         
         
+        if self.embedding_size != config.hidden_sizeq:
+            self.embed_proj = nn.Linear(self.embedding_size, config.hidden_size, bias=False)
+        
+        self.LayerNorm = DebertaLayerNorm(config.hidden_size, eps = config.layer_norm_eps)
+        self.dropout = StableDropout(config.hidden_dropout_prob) 
+        self.config = config
+        
+        self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
+        
+        
+    def forward(
+        self,
+        input_ids = None,
+        token_type_ids = None,
+        position_ids = None,
+        mask = None,
+        inputs_embeds = None,
+        past_key_values_length = 0,
+    ):
+        '''
+        
+        '''
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+            
+            
+        seq_length = input_shape[1]
+        if position_ids is None:
+            position_ids = self.position_ids[:, past_key_values_length: past_key_values_length+ seq_length]
         
         
         
@@ -1103,11 +1149,45 @@ class DebertaModel(DebertaPreTrainedModel):
 
         self.embeddings = DebertaEmbeddings(config)
         self.encoder = DebertaEncoder(config)
-        self.z_steps = 0
+        self.z_steps = 0 # 
         self.config = config
         self.init_weights()
+        
+    def get_input_embeddings(self):
+        return self.embeddings.word_embeddings
+    
+    def set_input_embeddings(self, new_embeddings):
+        self.embeddings.word_embeddings = new_embeddings
+    
+    def _prune_heads(self, heads_to_prune):
+        """
+        Prunes heads of the model. heads_to_prune: dict of {layer_num: list of heads to prune in this layer} See base
+        class PreTrainedModel
+        """
+        raise NotImplementedError("The prune function is not implemented in DeBERTa model.")
 
-
+    
+    @add_start_docstrings_to_model_forward(DEBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    @add_code_sample_docstrings(
+        tokenizer_class=_TOKENIZER_FOR_DOC,
+        checkpoint=_CHECKPOINT_FOR_DOC,
+        output_type=SequenceClassifierOutput,
+        config_class=_CONFIG_FOR_DOC,
+    )
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        past_key_values=None,
+    ):
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        
 
 
 
@@ -1122,18 +1202,125 @@ class DebertaForMaskedLM(DebertaPreTrainedModel):
         self.deberta = DebertaModel(config)
         self.cls = DebertaOnlyMLMHead(config)
 
-        self.init_weights()        
+        self.init_weights()      
+    
+    def get_output_embeddings(self):
+        pass
+    
+    def set_output_embeddings(self, new_embeddings):
+        pass
+     
+        
+        
+    @add_start_docstrings_to_model_forward(DEBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    @add_code_sample_docstrings(
+        tokenizer_class=_TOKENIZER_FOR_DOC,
+        checkpoint=_CHECKPOINT_FOR_DOC,
+        output_type=MaskedLMOutput,
+        config_class=_CONFIG_FOR_DOC,
+    )
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the masked language modeling loss. Indices should be in ``[-100, 0, ...,
+            config.vocab_size]`` (see ``input_ids`` docstring) Tokens with indices set to ``-100`` are ignored
+            (masked), the loss is only computed for the tokens with labels in ``[0, ..., config.vocab_size]``
+        """
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.deberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        
+        sequence_output = outputs[0]
+        prediction_scores = self.cls(sequence_output) # shape=  (bz, seqlen, d)
+        
+        masked_lm_loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss() # -100 index = padding token
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+        
+        
+        if not return_dict:
+            output = (prediction_scores,) + outputs[2:]
+            return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
+        
+        return MaskedLMOutput(
+            loss=masked_lm_loss,
+            logits=prediction_scores,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
         
         
 # copied from transformers.models.bert.BertPredictionHeadTransform with bert -> deberta
 class DebertaPredictionHeadTransform(nn.Module):
-    pass
+    '''
+    本质就是一个 FFN
+    '''
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        if isinstance(config.hidden_act, str):
+            self.transform_act_fn = ACT2FN[config.hidden_act]
+        else:
+            self.transform_act_fn = config.hidden_act
+        
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps = config.layer_norm_eps)
+    
+    def forward(self, hidden_states):
+        '''
+        这就是一个FFN的forward pass
+        '''
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.transform_act_fn(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states)
+        return hidden_states
 
 
 
 # copied from transformers.models.bert.BertLMPredictionHead with bert -> deberta
 class DebertaLMPredictionHead(nn.Module):
-    pass
+    '''
+    ##Intro
+        这实际上就是一个词表大小的分类器
+    '''
+    def __init__(self, config):
+        super().__init__()
+        self.transform = DebertaPredictionHeadTransform(config)
+        
+        # The output weights are the same as the input embeddings, but there is
+        # an output-only bias for each token.
+        self.decoder = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        
+        self.bias = nn.Parameter(torch.zeros(config.vocab_size)) # 每个token对应一个bias
+        
+        self.decoder.bias = self.bias
+        
+    def forward(self, hidden_states):
+        hidden_states = self.transform(hidden_states)
+        hidden_states = self.decoder(hidden_states)
+        return hidden_states
+        
 
 
 
@@ -1143,9 +1330,12 @@ class DebertaLMPredictionHead(nn.Module):
 class DebertaOnlyMLMHead(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.predictions = DebertaLMPredictionHead(config)
+        self.predictions = DebertaLMPredictionHead(config) # 分类头
 
     def forward(self, sequence_output):
+        '''
+        sequence_output.shape = (bz, seq_len, d)
+        '''
         prediction_scores = self.predictions(sequence_output)
         return prediction_scores
 
@@ -1178,7 +1368,71 @@ class DebertaForSequenceClassification(DebertaPreTrainedModel):
         self.dropout = StableDropout(drop_out)
 
         self.init_weights()
+        
+    
+    
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss. Indices should be in :obj:`[0, ...,
+            config.num_labels - 1]`. If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
+        outputs = self.deberta(
+            input_ids,
+            token_type_ids=token_type_ids, 
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        encoder_layer = outputs[0]
+        
+        
+        loss = None
+        
+        if labels is not None:
+            pass
+        
+        
+        
+        else:
+            pass
+        
+        
+        
+        
+        if not return_dict:
+            pass
+        
+        
+        return SequenceClassifierOutput(
+            loss = loss,
+            logits = logits,
+            hidden_states= outputs.hidden_states,
+            attentions = outputs.attentions
+        )
+        
+        
+        
+        
+        
 
 
 
@@ -1206,6 +1460,65 @@ class DebertaForTokenClassification(DebertaPreTrainedModel):
             param.requires_grad = False
 
         self.init_weights()
+        
+        
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the token classification loss. Indices should be in ``[0, ..., config.num_labels -
+            1]``.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.deberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+        
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output) # shape = (bz, seqlen, vocab_len)
+        
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            
+            
+            
+        else:
+            pass
+        
+        
+        
+        
+        if not return_dict:
+            pass
+        
+        
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 
 
