4e8eac9d96b6cebaa57c5a0afbbf149dd413a7ab
zhongyah <1016509070@qq.com>
2025-02-27 22:32:07 +0800

    update

--- model/deberta/deberta.py
+++ model/deberta/deberta.py
@@ -1845,6 +1845,102 @@ class DebertaForQuestionAnswering(DebertaPreTrainedModel):
         self.num_labels = config.num_labels
 
         self.deberta = DebertaModel(config)
-        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)
+        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels) # num_labels 一般取2， 一个start, 一个 end
 
         self.init_weights()
+        
+        
+        
+    @add_start_docstrings_to_model_forward(DEBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    @add_code_sample_docstrings(
+        tokenizer_class=_TOKENIZER_FOR_DOC,
+        checkpoint=_CHECKPOINT_FOR_DOC,
+        output_type=QuestionAnsweringModelOutput,
+        config_class=_CONFIG_FOR_DOC,
+    )
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        start_positions=None, # label 1
+        end_positions=None,  # label 2
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        
+        r"""
+        start_positions (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for position (index) of the start of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (:obj:`sequence_length`). Position outside of the
+            sequence are not taken into account for computing the loss.
+        end_positions (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for position (index) of the end of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (:obj:`sequence_length`). Position outside of the
+            sequence are not taken into account for computing the loss.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        
+        
+        outputs = self.deberta.forward(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        
+        sequence_output = outputs[0] # shape = (B, L, D)
+
+        logits = self.qa_outputs(sequence_output) # shape = (B, L, 2)
+        start_logits, end_logits = logits.split(1, dim=-1) # shape = (B, L ,1)
+        
+        start_logits = start_logits.squeeze(-1).contiguous() # shape = (B, L), 每个token所在的位置都对应着一个 0-1的logit， 代表该token是 start/end 的概率
+        end_logits = end_logits.squeeze(-1).contiguous()
+        
+        
+        total_loss = None
+        
+        if start_positions is not None and end_positions is not None:
+            # start_positions.shape = (B, )
+            # end_postions.shape = (B, )
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1: # e.g. (n_gpus, B, )
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1: # e.g. (n_gpus, B, )
+                end_positions = end_positions.squeeze(-1) # (n_gpus * B, )
+            
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            # start/end positions 超出序列长度
+            
+            ignored_index = start_logits.size(1) # seq_len
+            # clamp 用来限定范围
+            start_positions = torch.clamp(start_positions, 0, ignored_index)
+            end_positions = torch.clamp(end_positions, 0, ignored_index)
+            
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            
+            total_loss = (start_loss + end_loss) / 2
+            
+        
+        
+        if not return_dict:
+            output = (start_logits, end_logits) + outputs[1:]
+            return ((total_loss, ) + output) if total_loss is not None else output
+        
+        return QuestionAnsweringModelOutput(
+            loss=total_loss,
+            start_logits=start_logits,
+            end_logits=end_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+        
